d145d835112d2b4ae1fff8ada55e13ff8798fd62
Erubey Cazares <aecazares@hotmail.com>
2024-08-08 23:46:33 -0600

    API rest basic project

---
+++ schemas/movies.js
@@ -0,0 +1,29 @@
+const z = require('zod');
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required'
+  }),
+  year: z.number().int().positive().min(1900).max(2025),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(0),
+  poster: z.string().url(),
+  genre: z.array(
+    z.enum(['Action', 'Crime', 'Drama', 'Sci-Fi', 'Adventure', 'Romance', 'Animation', 'Biography', 'Fantasy'])
+  )
+});
+
+function validateMovie (object) {
+  return movieSchema.safeParse(object);
+}
+
+function validatePartialMovie (object) {
+  return movieSchema.partial().safeParse(object);
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+};
